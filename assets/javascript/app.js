c95632af7c45522301f06b41224f5d5fd499f502
Israel Niezen <israelniezen@Israels-MacBook-Pro.local>
2016-07-31 15:20:12 -0700

    setup all files, setup html, and trivia questions and answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,45 @@
+var questions = [{
+	question: "Which Team Won The 1986 World Cup?", 
+	choices: ["Argentina", "Germany", "England", "Italy"],
+	correctAnswer: 0
+}, {
+	question: "Who Is The All-Time Highest-Scoring Foreigner in The Bundesliga?", 
+	choices: ["Diego Maradona", "Pele", "Claudio Pizarro", "Jurgen Klinsman"],
+	correctAnswer: 2
+
+}, {
+
+	question: "What 2 Teams Make Up “El Clasico” In The Spanish League?",
+	choices: ["Atletico Madrid and Real Madrid","Bilbao and Barcelona","Sevilla and Milan",
+	"Barcelona and Real Madrid"],
+	correctAnswer: 3
+
+}, {
+
+question: "Which Team Lost The 1978 World Cup?",
+choices: ["Argentina","Netherlands","Germany","Brazil"],
+correctAnswer: 1
+
+}, {
+
+question: "Which Country Has Won The Most World Cups?",
+choices: ["Germany","Argentina","Brazil","Italy"],
+correctAnswer: 2
+}, {
+
+question: "Who is currently the top scorer in Barcelona?",
+choices: ["Ronaldo","Messi","Xavi","Suarez"],
+correctAnswer: 1
+
+}, {
+
+question: "Who is currently the top scorer in Real Madrid?",
+choices: ["Messi","Di Maria","Ronaldo","Higuain"],
+correctAnswer: 2
+}, {
+
+question: "Which team won the 1988 Eurocup?",
+choices: ["Germany","France","Italy","Netherlands"],
+correctAnswer: 3
+}];
+
